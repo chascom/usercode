d1d4e88951779aea080e41db6c6443e2653b7043
chasco <>
2012-04-24 00:27:21 +0000

    first commit

---
+++ InvisibleHiggs/Optimize.C
@@ -0,0 +1,537 @@
+#include "TLorentzVector.h"
+#include "TH1.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "TLorentzVector.h"
+#include <vector>
+#include <TVector2.h>
+#include "TVector3.h"
+#include <cmath>
+#include <TMath.h>
+
+void ReducedMET(TString Preselection, TString LEPTON_TYPE,TString OptimizedVariable,float EfficiencyMin, float SoverSqrtBMin){	
+	//filetypeMET
+	//TString filetypeMET = "d0met";
+	//TString LEPTONNAME = "13";
+	
+
+
+	std::cout<<"ok Opt"<<std::endl;
+	
+
+	
+
+	//THEHISTOGRAM
+	TH1F* h_MC_DYJetsToLL_0 = new TH1F("h_MC_DYJetsToLL_0","title",1,0,2);
+	h_MC_DYJetsToLL_0->Sumw2();
+	TH1F* h_MC_DYJetsToLL_1 = new TH1F("h_MC_DYJetsToLL_1","title",1,0,2);
+	h_MC_DYJetsToLL_1->Sumw2();
+	TH1F* h_MC_DYJetsToLL_2 = new TH1F("h_MC_DYJetsToLL_2","title",1,0,2);
+	h_MC_DYJetsToLL_2->Sumw2();
+	TH1F* h_MC_DYJetsToLL_3 = new TH1F("h_MC_DYJetsToLL_3","title",1,0,2);
+	h_MC_DYJetsToLL_3->Sumw2();
+	TH1F* h_MC_DYJetsToLL_4 = new TH1F("h_MC_DYJetsToLL_4","title",1,0,2);
+	h_MC_DYJetsToLL_4->Sumw2();
+	TH1F* h_MC_DYJetsToLL_5 = new TH1F("h_MC_DYJetsToLL_5","title",1,0,2);
+	h_MC_DYJetsToLL_5->Sumw2();
+	TH1F* h_MC_DYJetsToLL_6 = new TH1F("h_MC_DYJetsToLL_6","title",1,0,2);
+	h_MC_DYJetsToLL_6->Sumw2();
+	TH1F* h_MC_DYJetsToLL_7 = new TH1F("h_MC_DYJetsToLL_7","title",1,0,2);
+	h_MC_DYJetsToLL_7->Sumw2();
+	TH1F* h_MC_DYJetsToLL_8 = new TH1F("h_MC_DYJetsToLL_8","title",1,0,2);
+	h_MC_DYJetsToLL_8->Sumw2();
+	TH1F* h_MC_DYJetsToLL_9 = new TH1F("h_MC_DYJetsToLL_9","title",1,0,2);
+	h_MC_DYJetsToLL_9->Sumw2();
+	TH1F* h_MC_SingleTbar_s = new TH1F("h_MC_SingleTbar_s","title",1,0,2);
+	h_MC_SingleTbar_s->Sumw2();
+	TH1F* h_MC_SingleTbar_t = new TH1F("h_MC_SingleTbar_t","title",1,0,2);
+	h_MC_SingleTbar_t->Sumw2();
+	TH1F* h_MC_SingleTbar_tW = new TH1F("h_MC_SingleTbar_tW","title",1,0,2);
+	h_MC_SingleTbar_tW->Sumw2();
+	TH1F* h_MC_SingleT_s = new TH1F("h_MC_SingleT_s","title",1,0,2);
+	h_MC_SingleT_s->Sumw2();
+	TH1F* h_MC_SingleT_t = new TH1F("h_MC_SingleT_t","title",1,0,2);
+	h_MC_SingleT_t->Sumw2();
+	TH1F* h_MC_SingleT_tW = new TH1F("h_MC_SingleT_tW","title",1,0,2);
+	h_MC_SingleT_tW->Sumw2();
+	TH1F* h_MC_TTJets = new TH1F("h_MC_TTJets","title",1,0,2);
+	h_MC_TTJets->Sumw2();
+	TH1F* h_MC_WJetsToLNu = new TH1F("h_MC_WJetsToLNu","title",1,0,2);
+	h_MC_WJetsToLNu->Sumw2();
+	TH1F* h_MC_WW = new TH1F("h_MC_WW","title",1,0,2);
+	h_MC_WW->Sumw2();
+	TH1F* h_MC_WZ_0 = new TH1F("h_MC_WZ_0","title",1,0,2);
+	h_MC_WZ_0->Sumw2();
+	TH1F* h_MC_WZ_1 = new TH1F("h_MC_WZ_1","title",1,0,2);
+	h_MC_WZ_1->Sumw2();
+	TH1F* h_MC_ZZ_0 = new TH1F("h_MC_ZZ_0","title",1,0,2);
+	h_MC_ZZ_0->Sumw2();
+	TH1F* h_MC_ZZ_1 = new TH1F("h_MC_ZZ_1","title",1,0,2);
+	h_MC_ZZ_1->Sumw2();
+	
+
+		
+
+
+	//THEPROJECTION
+	MC_DYJetsToLL_0_tree->Project("h_MC_DYJetsToLL_0","1",Preselection);
+	MC_DYJetsToLL_1_tree->Project("h_MC_DYJetsToLL_1","1",Preselection);
+	MC_DYJetsToLL_2_tree->Project("h_MC_DYJetsToLL_2","1",Preselection);
+	MC_DYJetsToLL_3_tree->Project("h_MC_DYJetsToLL_3","1",Preselection);
+	MC_DYJetsToLL_4_tree->Project("h_MC_DYJetsToLL_4","1",Preselection);
+	MC_DYJetsToLL_5_tree->Project("h_MC_DYJetsToLL_5","1",Preselection);
+	MC_DYJetsToLL_6_tree->Project("h_MC_DYJetsToLL_6","1",Preselection);
+	MC_DYJetsToLL_7_tree->Project("h_MC_DYJetsToLL_7","1",Preselection);
+	MC_DYJetsToLL_8_tree->Project("h_MC_DYJetsToLL_8","1",Preselection);
+	MC_DYJetsToLL_9_tree->Project("h_MC_DYJetsToLL_9","1",Preselection);
+	MC_SingleTbar_s_tree->Project("h_MC_SingleTbar_s","1",Preselection);
+	MC_SingleTbar_t_tree->Project("h_MC_SingleTbar_t","1",Preselection);
+	MC_SingleTbar_tW_tree->Project("h_MC_SingleTbar_tW","1",Preselection);
+	MC_SingleT_s_tree->Project("h_MC_SingleT_s","1",Preselection);
+	MC_SingleT_t_tree->Project("h_MC_SingleT_t","1",Preselection);
+	MC_SingleT_tW_tree->Project("h_MC_SingleT_tW","1",Preselection);
+	MC_TTJets_tree->Project("h_MC_TTJets","1",Preselection);
+	MC_WJetsToLNu_tree->Project("h_MC_WJetsToLNu","1",Preselection);
+	MC_WW_tree->Project("h_MC_WW","1",Preselection);
+	MC_WZ_0_tree->Project("h_MC_WZ_0","1",Preselection);
+	MC_WZ_1_tree->Project("h_MC_WZ_1","1",Preselection);
+	MC_ZZ_0_tree->Project("h_MC_ZZ_0","1",Preselection);
+	MC_ZZ_1_tree->Project("h_MC_ZZ_1","1",Preselection);
+	
+	
+	
+	
+	std::cout<<"ok PROJECTION"<<std::endl;
+	
+
+	//THENUMBEROFEVENTS
+	float N_MC_DYJetsToLL_0 = h_MC_DYJetsToLL_0->Integral();
+	float N_MC_DYJetsToLL_1 = h_MC_DYJetsToLL_1->Integral();
+	float N_MC_DYJetsToLL_2 = h_MC_DYJetsToLL_2->Integral();
+	float N_MC_DYJetsToLL_3 = h_MC_DYJetsToLL_3->Integral();
+	float N_MC_DYJetsToLL_4 = h_MC_DYJetsToLL_4->Integral();
+	float N_MC_DYJetsToLL_5 = h_MC_DYJetsToLL_5->Integral();
+	float N_MC_DYJetsToLL_6 = h_MC_DYJetsToLL_6->Integral();
+	float N_MC_DYJetsToLL_7 = h_MC_DYJetsToLL_7->Integral();
+	float N_MC_DYJetsToLL_8 = h_MC_DYJetsToLL_8->Integral();
+	float N_MC_DYJetsToLL_9 = h_MC_DYJetsToLL_9->Integral();
+	float N_MC_SingleTbar_s = h_MC_SingleTbar_s->Integral();
+	float N_MC_SingleTbar_t = h_MC_SingleTbar_t->Integral();
+	float N_MC_SingleTbar_tW = h_MC_SingleTbar_tW->Integral();
+	float N_MC_SingleT_s = h_MC_SingleT_s->Integral();
+	float N_MC_SingleT_t = h_MC_SingleT_t->Integral();
+	float N_MC_SingleT_tW = h_MC_SingleT_tW->Integral();
+	float N_MC_TTJets = h_MC_TTJets->Integral();
+	float N_MC_WJetsToLNu = h_MC_WJetsToLNu->Integral();
+	float N_MC_WW = h_MC_WW->Integral();
+	float N_MC_WZ_0 = h_MC_WZ_0->Integral();
+	float N_MC_WZ_1 = h_MC_WZ_1->Integral();
+	float N_MC_ZZ_0 = h_MC_ZZ_0->Integral();
+	float N_MC_ZZ_1 = h_MC_ZZ_1->Integral();
+	
+	
+
+
+	
+	//THESUMOFBACKGROUND //bkgd =
+	float bkgd = N_MC_DYJetsToLL_0 + N_MC_DYJetsToLL_1 + N_MC_DYJetsToLL_2 + N_MC_DYJetsToLL_3 + N_MC_DYJetsToLL_4 + N_MC_DYJetsToLL_5 + N_MC_DYJetsToLL_6 + N_MC_DYJetsToLL_7 + N_MC_DYJetsToLL_8 + N_MC_DYJetsToLL_9 + N_MC_SingleTbar_s + N_MC_SingleTbar_t + N_MC_SingleTbar_tW + N_MC_SingleT_s + N_MC_SingleT_t + N_MC_SingleT_tW + N_MC_TTJets + N_MC_WJetsToLNu + N_MC_WW + N_MC_WZ_0 + N_MC_WZ_1; //bkgd =
+
+
+	
+	
+	//THESUMOFSIGNAL //sig =
+	float sig = N_MC_ZZ_0 + N_MC_ZZ_1; //sig =
+
+	
+		std::cout<<bkgd<<" number "<<sig<<std::endl;
+	
+
+	float sig2n = sig/sqrt(bkgd);
+	
+	
+
+	float sig2ncut4 = 0;
+	float sigcut4 = 0;
+	float bkgd4 = 0;
+	float sig2ncut1 = 0;
+	float sigcut1 = 0;
+	float bkgd1 = 0;
+	float sig2ncut3 = 0;
+	float sigcut3 = 0;
+	float bkgd3 = 0;
+	float sigEFF1 = 0;
+	float bkgdREJ1 = 0;
+	float sigEFF3 = 0;
+	float bkgdREJ3 = 0;
+	float sigEFF4 = 0;
+	float bkgdREJ4 = 0;
+	TString Local_Max_T;
+	float sigcut0 = 0.0; //nonzero sig in zero back
+	vector <float> signalEFF;
+	vector <float> backgdREJ;
+	vector <float> signal2noise;
+	vector <float> pointstorage_signalEFF;
+	vector <float> pointstorage_backgdREJ;
+	vector <float> pointstorage_signal2noise;
+	vector <float> pointstorage_sigcut;
+	
+	vector <string> opt_store;
+	
+	
+	float spanii = 3.0;
+	float spankk = 3.0;
+	float spanrr = 3.0;
+	float spanjj = 60.0;
+	float Nii = 2;
+	float Nkk = 2;
+	float Nrr = 2;
+	float Njj = 2;
+	
+	TString SSfinal_T = "000000000";
+	Local_Max_T = "000000000";
+	TString BOTHfinal_T = "000000000";
+	
+	//10,10,5,6
+	
+	for (int ii=0; 1.0*ii<Nii+1; ii++) //uncertainty weight index 2-3
+	{
+		std::ostringstream strii;
+		strii << 0 + spanii*ii/Nii;
+		std::string Wunc = strii.str(); 
+		
+
+								 //bisector weight index 1-2
+		for (int kk=0; 1.0*kk<Nkk+1; kk++)
+		{
+			std::ostringstream strkk;
+			strkk << 0 + spankk*kk/Nkk;
+			std::string Wbis = strkk.str(); 
+			
+								 //recoil weight index
+			for (int rr=0; 1.0*rr<Nrr+1; rr++)
+			{
+				std::ostringstream strrr;
+				strrr << 0 + spanrr*rr/Nrr;
+				std::string Wrec = strrr.str(); 
+				
+								 // MET cut index 30-80 GeV
+				for (int jj=0; 1.0*jj<Njj+1; jj++) 
+				{
+
+				std::ostringstream strjj;
+				strjj << 20 + spanjj*jj/Njj;
+				std::string RMET = strjj.str(); 
+							
+std::cout<<ii<<jj<<kk<<rr<<std::endl;
+
+string MET ="sqrt((dilepPROJLong + "+ Wrec +"*recoilPROJLong + "+ Wunc +"*uncertPROJLong)*(dilepPROJLong + "+ Wrec +"*recoilPROJLong + "+ Wunc +"*uncertPROJLong)*(dilepPROJLong + "+ Wrec +"*recoilPROJLong + "+ Wunc +"*uncertPROJLong >= 0) + 0*(dilepPROJLong + "+ Wrec +"*recoilPROJLong + "+ Wunc +"*uncertPROJLong < 0) + "+ Wbis +"*(dilepPROJPerp + "+ Wrec +"*recoilPROJPerp + "+ Wunc +"*uncertPROJPerp)*(dilepPROJPerp + "+ Wrec +"*recoilPROJPerp + "+ Wunc +"*uncertPROJPerp)*(dilepPROJPerp + "+ Wrec +"*recoilPROJPerp + "+ Wunc +"*uncertPROJPerp >= 0) + 0*(dilepPROJPerp + "+ Wrec +"*recoilPROJPerp + "+ Wunc +"*uncertPROJPerp < 0))";
+TString TMET = "*(" + MET + ">" + RMET + ")";
+
+string opt_simp ="Perp: " + Wbis + " Recoil: " + Wrec + " Uncertainty: " + Wunc + " redMETcut: " + RMET;
+std::cout<<opt_simp<<std::endl;
+		
+					
+					//THEPOSTCUTPROJECTION
+					MC_DYJetsToLL_0_tree->Project("h_MC_DYJetsToLL_0","1",Preselection+TMET);
+					MC_DYJetsToLL_1_tree->Project("h_MC_DYJetsToLL_1","1",Preselection+TMET);
+					MC_DYJetsToLL_2_tree->Project("h_MC_DYJetsToLL_2","1",Preselection+TMET);
+					MC_DYJetsToLL_3_tree->Project("h_MC_DYJetsToLL_3","1",Preselection+TMET);
+					MC_DYJetsToLL_4_tree->Project("h_MC_DYJetsToLL_4","1",Preselection+TMET);
+					MC_DYJetsToLL_5_tree->Project("h_MC_DYJetsToLL_5","1",Preselection+TMET);
+					MC_DYJetsToLL_6_tree->Project("h_MC_DYJetsToLL_6","1",Preselection+TMET);
+					MC_DYJetsToLL_7_tree->Project("h_MC_DYJetsToLL_7","1",Preselection+TMET);
+					MC_DYJetsToLL_8_tree->Project("h_MC_DYJetsToLL_8","1",Preselection+TMET);
+					MC_DYJetsToLL_9_tree->Project("h_MC_DYJetsToLL_9","1",Preselection+TMET);
+					MC_SingleTbar_s_tree->Project("h_MC_SingleTbar_s","1",Preselection+TMET);
+					MC_SingleTbar_t_tree->Project("h_MC_SingleTbar_t","1",Preselection+TMET);
+					MC_SingleTbar_tW_tree->Project("h_MC_SingleTbar_tW","1",Preselection+TMET);
+					MC_SingleT_s_tree->Project("h_MC_SingleT_s","1",Preselection+TMET);
+					MC_SingleT_t_tree->Project("h_MC_SingleT_t","1",Preselection+TMET);
+					MC_SingleT_tW_tree->Project("h_MC_SingleT_tW","1",Preselection+TMET);
+					MC_TTJets_tree->Project("h_MC_TTJets","1",Preselection+TMET);
+					MC_WJetsToLNu_tree->Project("h_MC_WJetsToLNu","1",Preselection+TMET);
+					MC_WW_tree->Project("h_MC_WW","1",Preselection+TMET);
+					MC_WZ_0_tree->Project("h_MC_WZ_0","1",Preselection+TMET);
+					MC_WZ_1_tree->Project("h_MC_WZ_1","1",Preselection+TMET);
+					MC_ZZ_0_tree->Project("h_MC_ZZ_0","1",Preselection+TMET);
+					MC_ZZ_1_tree->Project("h_MC_ZZ_1","1",Preselection+TMET);
+
+
+					
+					//THEPOSTCUTNUMBEROFEVENTS
+					float Ncut_MC_DYJetsToLL_0 = h_MC_DYJetsToLL_0->Integral();
+					float Ncut_MC_DYJetsToLL_1 = h_MC_DYJetsToLL_1->Integral();
+					float Ncut_MC_DYJetsToLL_2 = h_MC_DYJetsToLL_2->Integral();
+					float Ncut_MC_DYJetsToLL_3 = h_MC_DYJetsToLL_3->Integral();
+					float Ncut_MC_DYJetsToLL_4 = h_MC_DYJetsToLL_4->Integral();
+					float Ncut_MC_DYJetsToLL_5 = h_MC_DYJetsToLL_5->Integral();
+					float Ncut_MC_DYJetsToLL_6 = h_MC_DYJetsToLL_6->Integral();
+					float Ncut_MC_DYJetsToLL_7 = h_MC_DYJetsToLL_7->Integral();
+					float Ncut_MC_DYJetsToLL_8 = h_MC_DYJetsToLL_8->Integral();
+					float Ncut_MC_DYJetsToLL_9 = h_MC_DYJetsToLL_9->Integral();
+					float Ncut_MC_SingleTbar_s = h_MC_SingleTbar_s->Integral();
+					float Ncut_MC_SingleTbar_t = h_MC_SingleTbar_t->Integral();
+					float Ncut_MC_SingleTbar_tW = h_MC_SingleTbar_tW->Integral();
+					float Ncut_MC_SingleT_s = h_MC_SingleT_s->Integral();
+					float Ncut_MC_SingleT_t = h_MC_SingleT_t->Integral();
+					float Ncut_MC_SingleT_tW = h_MC_SingleT_tW->Integral();
+					float Ncut_MC_TTJets = h_MC_TTJets->Integral();
+					float Ncut_MC_WJetsToLNu = h_MC_WJetsToLNu->Integral();
+					float Ncut_MC_WW = h_MC_WW->Integral();
+					float Ncut_MC_WZ_0 = h_MC_WZ_0->Integral();
+					float Ncut_MC_WZ_1 = h_MC_WZ_1->Integral();
+					float Ncut_MC_ZZ_0 = h_MC_ZZ_0->Integral();
+					float Ncut_MC_ZZ_1 = h_MC_ZZ_1->Integral();
+
+					
+					
+					//THESUMOFPOSTCUTBACKGROUND //float bkgdcut = 
+					float bkgdcut = Ncut_MC_DYJetsToLL_0 + Ncut_MC_DYJetsToLL_1 + Ncut_MC_DYJetsToLL_2 + Ncut_MC_DYJetsToLL_3 + Ncut_MC_DYJetsToLL_4 + Ncut_MC_DYJetsToLL_5 + Ncut_MC_DYJetsToLL_6 + Ncut_MC_DYJetsToLL_7 + Ncut_MC_DYJetsToLL_8 + Ncut_MC_DYJetsToLL_9 + Ncut_MC_SingleTbar_s + Ncut_MC_SingleTbar_t + Ncut_MC_SingleTbar_tW + Ncut_MC_SingleT_s + Ncut_MC_SingleT_t + Ncut_MC_SingleT_tW + Ncut_MC_TTJets + Ncut_MC_WJetsToLNu + Ncut_MC_WW + Ncut_MC_WZ_0 + Ncut_MC_WZ_1; //float bkgdcut = 
+
+
+					
+					
+					//THESUMOFPOSTCUTSIGNAL //float sigcut =  //RR
+					float sigcut = Ncut_MC_ZZ_0 + Ncut_MC_ZZ_1; //float sigcut =  //RR
+
+
+					std::cout<<bkgdcut<<" number "<<sigcut<<std::endl;
+					/*if (bkgdcut == 0)
+					{
+						std::cout<<"ZERO BACKGROUND: "<<cut_name_T<<std::endl;
+						if (sigcut != 0)
+						{
+							std::cout<<"ZERO BACKGROUND,|||| NONZERO SIGNAL: "<<cut_name_T<<std::endl;
+							if(sigcut > sigcut0){
+								float sigcut0 = sigcut;
+								TString Nonzero_T = cut_name;
+							}
+						}
+						continue;
+					}*/
+					//else
+					//{
+					float sig2ncut = sigcut/sqrt(bkgdcut);
+					float sigEFF = sigcut/sig;
+					float bkgdREJ = 1-bkgdcut/bkgd;
+						
+					signalEFF.push_back(sigEFF); //puts stuff in vector to be used later.
+					backgdREJ.push_back(bkgdREJ);
+					signal2noise.push_back(sig2ncut);
+						
+						//std::cout<<"NONZERO BACKGROUND"<<std::endl;
+					//}
+
+					if ((sig2ncut > SoverSqrtBMin)*(sigEFF > EfficiencyMin)){ //stores values to compare with Tgraph output, above thresholds
+						pointstorage_signalEFF.push_back(sigEFF);
+						pointstorage_backgdREJ.push_back(bkgdREJ);
+						pointstorage_signal2noise.push_back(sig2ncut);
+						pointstorage_sigcut.push_back(sigcut);
+						opt_store.push_back(opt_simp);
+						
+						if(sig2ncut > sig2ncut4)//maximize S/sqrt(B) in storing region
+						{
+						sig2ncut4 = sig2ncut;
+						sigcut4 = sigcut;
+						bkgd4 = bkgdcut;
+						sigEFF4 = sigEFF;
+						bkgdREJ4 = bkgdREJ;
+						Local_Max_T = opt_simp;
+						}
+					}
+					
+
+					if (sig2ncut > sig2ncut1)// maximize S/sqrt(B)
+					{
+						sig2ncut1 = sig2ncut;
+						sigcut1 = sigcut;
+						bkgd1 = bkgdcut;
+						sigEFF1 = sigEFF;
+						bkgdREJ1 = bkgdREJ;
+						SSfinal_T = opt_simp;
+					}
+					/*if (RR > RR2)// maximize Signal
+					{
+						float RR2 = RR;
+						float SS2 = SS;
+						float BB2 = Back;
+						TString RRfinal_T = cut_nameadd;
+						TString RRfinal2_T = cut_name;
+					}*/
+								 // maximize S/sqrt(B) w/ S > 3
+					if ((sig2ncut > sig2ncut3)  && (sigcut > 3))
+					{
+						sig2ncut3 = sig2ncut;
+						sigcut3 = sigcut;
+						bkgd3 = bkgdcut;
+						sigEFF3 = sigEFF;
+						bkgdREJ3 = bkgdREJ;
+						BOTHfinal_T = opt_simp;
+					}
+								 //maximize S/sqrt(B) w/ B >= 1
+					/*if ((SS > SS4) && (Back < 1))
+					{
+						float SS4 = SS;
+						float RR4 = RR;
+						float BB4 = Back;
+						TString Backfinal_T = cut_nameadd;
+						TString Backfinal2_T = cut_name;
+					}*/
+					//std::cout<<"MET"<<str_jj<<" SS "<<SS<<std::endl;
+					//std::cout<<"RR "<<str_ii2<<str_ii<<" "<<RR<<std::endl;
+
+					//std::cout<<cut_name_T<<std::endl;
+				//}//uu
+				//std::cout<<"uu"<<std::endl;
+				//}//cc
+				//std::cout<<"cc"<<std::endl;
+				//}//nn
+				//std::cout<<"nn"<<std::endl;
+				}//jj
+				//std::cout<<"jj"<<"endloop"<<std::endl;
+			}
+			//std::cout<<"after1"<<std::endl;
+		}
+		//std::cout<<"after2"<<std::endl;
+		if ((ii == 2) || (ii == 3)) std::cout<<"ii"<<std::endl;
+	}
+	
+
+	
+	ofstream myfile;
+    myfile.open ("/home/chasco/Documents/Trees/Optimization/CutFlow_"+OptimizedVariable+"_"+LEPTON_TYPE+".txt");    
+
+
+	myfile << "\n\n";
+myfile << SSfinal_T;
+myfile << "\t S/sqrt(B): ";
+myfile << sig2ncut1;
+myfile << "\t S: ";
+myfile << sigcut1;
+myfile << "\t B: ";
+myfile << bkgd1;
+myfile << "\t EFF: ";
+myfile << sigEFF1;
+myfile << "\t REJ: ";
+myfile << bkgdREJ1;
+myfile << "\tmax ratio\n\n";
+	
+	
+						 
+	myfile << BOTHfinal_T;
+myfile << "\t S/sqrt(B): ";
+myfile << sig2ncut3;
+myfile << "\t S: ";
+myfile << sigcut3;
+myfile << "\t B: ";
+myfile << bkgd3;
+myfile << "\t EFF: ";
+myfile << sigEFF3;
+myfile << "\t REJ: ";
+myfile << bkgdREJ3;
+myfile << "\tmax ratio w/ S>3\n\n";
+	
+	
+	
+	myfile << Local_Max_T;
+myfile << "\t S/sqrt(B): ";
+myfile << sig2ncut4;
+myfile << "\t S: ";
+myfile << sigcut4;
+myfile << "\t B: ";
+myfile << bkgd4;
+myfile << "\t EFF: ";
+myfile << sigEFF4;
+myfile << "\t REJ: ";
+myfile << bkgdREJ4;
+myfile << "\tmax ratio in specified range\n\n";
+	
+	
+	int pointstorage_size = pointstorage_signalEFF.size();
+	for (int sindex = 0; sindex < pointstorage_size; ++sindex){
+		myfile << "\n\n";
+myfile << "Opt: ";
+myfile << opt_store[sindex];
+myfile << "\tnumberSIGevents:";
+myfile << pointstorage_sigcut[sindex];
+myfile << "\tSignalEFF: ";
+myfile << pointstorage_signalEFF[sindex];
+myfile << "\tBackgdREJ: ";
+myfile << pointstorage_backgdREJ[sindex];
+myfile << "\tSig2Noise: ";
+myfile << pointstorage_signal2noise[sindex];
+	} 
+	
+	myfile.close();
+	
+
+	const int vectorsize = signalEFF.size();
+	float signalEFF_forplot[vectorsize];
+	float backgdREJ_forplot[vectorsize];
+	float signal2noise_forplot[vectorsize];
+	for (int vindex = 0; vindex < vectorsize; ++vindex){
+		signalEFF_forplot[vindex] = signalEFF[vindex];
+		backgdREJ_forplot[vindex] = backgdREJ[vindex];
+		//backgdREJ_forplot[vindex] = 1;
+		signal2noise_forplot[vindex] = signal2noise[vindex];
+	}
+
+TCanvas *c1 = new TCanvas("c1","Eff and Rej",200,10,700,500);
+zzgr = new TGraph(vectorsize, backgdREJ_forplot, signalEFF_forplot);
+//zzgr->SetLineColor(2);
+//zzgr->SetLineWidth(4);
+zzgr->SetMarkerColor(4);
+zzgr->SetMarkerStyle(21);
+zzgr->SetMarkerSize(.3);
+zzgr->SetTitle("Sig Eff vs Bkgd Rej");
+zzgr->GetXaxis()->SetTitle("Background Rejection");
+zzgr->GetYaxis()->SetTitle("Signal Efficiency");
+//zzgr->GetXaxis()->SetRangeUser(0,80);
+//zzgr->GetYaxis()->SetRangeUser(0,0.1);
+//zzgr->Draw("ACP");
+zzgr->Draw("AP");
+c1 -> SaveAs("EFFvsREJ_"+OptimizedVariable+"_"+LEPTON_TYPE+".png");
+
+
+TCanvas *c2 = new TCanvas("c2","Eff and Sig to Noise",200,10,700,500);
+zzgr1 = new TGraph(vectorsize, signal2noise_forplot, signalEFF_forplot);
+//zzgr1->SetLineColor(2);
+//zzgr1->SetLineWidth(4);
+zzgr1->SetMarkerColor(4);
+zzgr1->SetMarkerStyle(21);
+zzgr1->SetMarkerSize(.3);
+zzgr1->SetTitle("Sig Eff vs Sig/sqrt(B)");
+zzgr1->GetXaxis()->SetTitle("Signal/sqrt(Bkgd)");
+zzgr1->GetYaxis()->SetTitle("Signal Efficiency");
+//zzgr1->GetXaxis()->SetRangeUser(0,80);
+//zzgr1->GetYaxis()->SetRangeUser(0,0.1);
+//zzgr1->Draw("ACP");
+zzgr1->Draw("AP");
+c2 -> SaveAs("EFFvsSOSB_"+OptimizedVariable+"_"+LEPTON_TYPE+".png");
+
+TIter next(gDirectory->GetList()); //Deletes histograms to avoid memory leaks
+       TObject* obj;
+       while(obj= (TObject*)next()){
+               if(obj->InheritsFrom(TH1::Class())){
+                       obj->Delete();
+               }
+       }
+
+}
+
+void Optimize(){
+
+gROOT->ProcessLine(".x LoadRootFiles.C");
+
+TString Preselection_Zdimuons = LUM + "*CrossSection*(1/NumGenEvents)*puweight*(l1_id*l2_id == -13*13)";
+TString Preselection_Zdielectrons = LUM + "*CrossSection*(1/NumGenEvents)*puweight*(l1_id*l2_id == -11*11)";
+TString Preselection_Z = LUM + "*CrossSection*(1/NumGenEvents)*puweight*((l1_id*l2_id == -11*11) + (l1_id*l2_id == -13*13))";
+
+ReducedMET(Preselection_Zdimuons,"MUON","d0",0.3,0.4); //MUONS
+ReducedMET(Preselection_Zdielectrons,"ELEC","d0",0.35,0.4); //ELECTRONS
+ReducedMET(Preselection_Z,"BOTH","d0",0.35,0.4);
+	
+gROOT->ProcessLine(".q;");
+
+}
---
+++ InvisibleHiggs/Write_Optimization_Code.py
@@ -0,0 +1,128 @@
+import os
+import sys
+
+directory = '/home/chasco/Documents/Trees/Rootfiles/Modified/'
+
+list_of_root = os.popen('ls '+directory+'*.root').readlines() #saves list of root files as vector
+
+new_list = []
+for x in list_of_root: #fix up root file names for use in script
+	if "MC_" in x:
+		if ("TauTau" not in x) and ("ZH" not in x): #temporary for TauTau?
+			new_list.append((x.replace('\n','')).replace(directory,''))
+list_of_root = new_list
+
+list_of_root_notype = []
+for xx in list_of_root: #remove '.root' for name use in script
+	list_of_root_notype.append(xx.replace('.root',''))
+	
+list_of_BKGD = []
+list_of_SIG = []
+for xxx in list_of_root_notype:
+	if (('ZZ' in xxx) or ('ZH' in xxx) or ('TauTau' in xxx)): #separates signal into one vector and background into another
+		if 'ZZ' in xxx:
+			list_of_SIG.append(xxx)
+	else:
+		list_of_BKGD.append(xxx)
+
+str_of_BKGD = ((((str(list_of_BKGD).replace('[','')).replace(']','')).replace(',',' +')).replace("'",'')).replace('MC_','N_MC_') #constructs sums of integrals
+str_of_SIG = ((((str(list_of_SIG).replace('[','')).replace(']','')).replace(',',' +')).replace("'",'')).replace('MC_','N_MC_')
+
+str_of_BKGD_cut = str_of_BKGD.replace('N_MC_','Ncut_MC_')
+str_of_SIG_cut = str_of_SIG.replace('N_MC_','Ncut_MC_')
+
+print str_of_BKGD_cut
+print str_of_SIG_cut
+
+MC_name = ['MC_ZH150', 'MC_ZZ', 'MC_WZ', 'MC_WW', 'MC_TTJets', 'MC_SingleT_s', 'MC_SingleTbar_s', 'MC_SingleT_t', 'MC_SingleTbar_t', 'MC_SingleT_tW', 'MC_SingleTbar_tW', 'MC_WJetsToLNu', 'MC_DYJetsToLL', 'MC_DYJetsToTauTau']
+MC_CrossSection = [0.01171, 5.9, 18.2, 43.0, 165.0, 3.19, 1.44, 41.92, 22.65, 7.87, 7.87, 31314.0, 3048.0, 1.0] #Must be same length as MC_name and every entry must correspond
+CorrectN = -1 #default
+
+templatefile_load = open('LoadRootFiles_TEMP.C','r') #opens template of branch adding (this is the file you edit)
+newfile_load = open('LoadRootFiles.C','w') #open write file for each root file
+
+CASTOR = 0	####### MAKE A CASTOR OPTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	
+COPYCUT = '"(ln==0)*(Cosmic==0)*(abs(Mass_Z - 91.2)<10)*(Pt_Z>30)*(DeltaPhi_metjet>0.5)*(Pt_J1 < 30)*(DeltaPhi_metjet > 0.5)"'
+#PRECUT = '"CrossSection*puweight*(1/NumGenEvents)"+LEPTON_TYPE'
+PRECUT = 'Preselection'
+METCUT = 'TMET'
+LUM = '4653'
+	
+for line in templatefile_load: #loop over lines in loader template file
+	newfile_load.write(line)
+	if "//THETFILE" in line:
+		for F in range(len(list_of_root)): #loop over position of root files in vector, opens files
+			lineTHETFILE = line.replace("//THETFILE", 'TFile '+list_of_root_notype[F]+'_f("'+directory+list_of_root[F]+'");')
+			newfile_load.write(lineTHETFILE)
+	if "//THEGETTREE" in line:
+		for F in range(len(list_of_root)): #locates trees
+			lineTHEGETTREE = line.replace("//THEGETTREE", 'TTree *'+list_of_root_notype[F]+'_pre_tree = (TTree *)'+list_of_root_notype[F]+'_f->Get("data");')
+			newfile_load.write(lineTHEGETTREE)
+	if "//THECOPYTREE" in line:
+		for F in range(len(list_of_root)): #copies trees with cut to speed up opt script
+			lineTHECOPYTREE = line.replace("//THECOPYTREE",'TFile *'+list_of_root_notype[F]+'_f2 = new TFile("'+directory+'COPYSTORE/new_'+list_of_root[F]+'","RECREATE");')
+			lineTHECOPYTREE2 = line.replace("//THECOPYTREE",'TTree * '+list_of_root_notype[F]+'_tree = '+list_of_root_notype[F]+'_pre_tree -> CopyTree(COPYCUT);')
+			newfile_load.write(lineTHECOPYTREE)
+			newfile_load.write(lineTHECOPYTREE2)
+	if "//COPYCUTSTRING" in line:
+		lineCOPYCUT = line.replace("//COPYCUTSTRING",'TString COPYCUT = ' + COPYCUT + ';')
+		newfile_load.write(lineCOPYCUT)
+	if "//LUMINOSITY" in line:
+		lineLUM = line.replace("//LUMINOSITY",'TString LUM = "' + LUM + '";')
+		newfile_load.write(lineLUM)
+	
+newfile_load.close()
+templatefile_load.close()
+
+templatefile_opt = open('Optimize_TEMP.C','r')
+newfile_opt = open('Optimize.C','w')
+for line in templatefile_opt:
+	newfile_opt.write(line)
+	if "//THEHISTOGRAM" in line: #writes list of histogram declarations
+		for F in range(len(list_of_root)):
+			lineTHEHISTOGRAM = line.replace("//THEHISTOGRAM", 'TH1F* h_'+list_of_root_notype[F]+' = new TH1F("h_'+list_of_root_notype[F]+'","title",1,0,2);')
+			lineTHEHISTOGRAM2 = line.replace("//THEHISTOGRAM", 'h_'+list_of_root_notype[F]+'->Sumw2();')
+			newfile_opt.write(lineTHEHISTOGRAM)
+			newfile_opt.write(lineTHEHISTOGRAM2)
+
+	if "//THEPROJECTION" in line: #writes projection commands
+		for F in range(len(list_of_root)):
+			lineTHEPROJECTION = line.replace("//THEPROJECTION", list_of_root_notype[F]+'_tree->Project("h_'+list_of_root_notype[F]+'","1",'+PRECUT+');')
+			newfile_opt.write(lineTHEPROJECTION)
+			
+	if "//THENUMBEROFEVENTS" in line: #writes declaration of MC distribution integrals
+		for F in range(len(list_of_root)):
+			lineTHENUMBEROFEVENTS = line.replace("//THENUMBEROFEVENTS", 'float N_'+list_of_root_notype[F]+' = h_' +list_of_root_notype[F]+ '->Integral();')
+			newfile_opt.write(lineTHENUMBEROFEVENTS)
+			
+	if "//THESUMOFBACKGROUND" in line: #writes background summing
+		lineTHESUMOFBACKGROUND = line.replace("//THESUMOFBACKGROUND", 'float bkgd = ' + str_of_BKGD + ';')
+		newfile_opt.write(lineTHESUMOFBACKGROUND)
+		
+	if "//THESUMOFSIGNAL" in line:
+		lineTHESIGNAL = line.replace('//THESUMOFSIGNAL', 'float sig = ' + str_of_SIG + ';')
+		newfile_opt.write(lineTHESIGNAL)
+			
+	if "//THEPOSTCUTPROJECTION" in line: #within loop, writes projection with additional MET cut
+		for F in range(len(list_of_root)):
+			lineTHEPOSTCUTPROJECTION = line.replace("//THEPOSTCUTPROJECTION", list_of_root_notype[F]+'_tree->Project("h_'+list_of_root_notype[F]+'","1",'+PRECUT+'+'+METCUT+');')
+			newfile_opt.write(lineTHEPOSTCUTPROJECTION)
+			
+	if "//THEPOSTCUTNUMBEROFEVENTS" in line: #within loop, writes integrals
+		for F in range(len(list_of_root)):
+			lineTHEPOSTCUTNUMBEROFEVENTS = line.replace("//THEPOSTCUTNUMBEROFEVENTS", 'float Ncut_'+list_of_root_notype[F]+' = h_' +list_of_root_notype[F]+ '->Integral();')
+			newfile_opt.write(lineTHEPOSTCUTNUMBEROFEVENTS)
+			
+	if "//THESUMOFPOSTCUTBACKGROUND" in line: #writes background summing
+		lineTHESUMOFBACKGROUND = line.replace("//THESUMOFPOSTCUTBACKGROUND", 'float bkgdcut = ' + str_of_BKGD_cut + ';')
+		newfile_opt.write(lineTHESUMOFBACKGROUND)
+		
+	if "//THESUMOFPOSTCUTSIGNAL" in line:
+		lineTHESIGNAL = line.replace('//THESUMOFPOSTCUTSIGNAL', 'float sigcut = ' + str_of_SIG_cut + ';')
+		newfile_opt.write(lineTHESIGNAL)
+#rootprocess.write('gROOT->ProcessLine(".q");\n}')
+#rootprocess.close()
+#newfile.close()
+#os.system('root -l ROOTPROCESS')
+#os.system('rm sample_*')
+
