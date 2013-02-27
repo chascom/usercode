8e3ff7035744e7e2250bb867ade4b61e9e4f2304
chasco <>
2013-02-27 03:46:52 +0000

    quick code for checking cross section at a cut level.

---
+++ Plots_Limits/CStable.py
@@ -0,0 +1,15 @@
+import numpy
+import array
+import random
+import os
+import sys
+import math
+# from Percents_elec import *
+# from Percents_muon import *
+# from YieldsSyst_elec import *
+# from YieldsSyst_muon import *
+#from Yields_elec import *
+from Yields_alllep import *
+
+for y in range(len(Yalllep)):
+	print Yalllep[y][0],(Yalllep[y][3])/5.035
