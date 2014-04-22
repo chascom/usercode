2992871cae3cb52b50c7847b812acc0dcb5f742a
Matthew <chasco@cern.ch>
2014-04-22 20:53:40 +0200

    removes run entries with missing components

--- CSCVAL/CSCVALDefectRemover.py
+++ CSCVAL/CSCVALDefectRemover.py
@@ -1,57 +1,104 @@
 import os 
 import sys
-#import runlist_empty
-from  runlist_empty import *
-#from  unfinished_empty2 import *
-import numpy
-import array
-import random
-# copy runlogs from appropriate data stream before running
+
+d = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/results'
+types=[]
+allinfo=[]
+print "IS THIS RUNNING?"
+filelist = os.listdir(d)
+reducedfilelist = []
+for x in filelist:
+	if (x.replace('run','') >= '204554'): #beginning point
+		reducedfilelist.append(x)
 
 
-print DOUBLE
-print SINGLE
+for x in reducedfilelist: #loop over run files
+	if 'run' not in x:
+		continue
+	run = int(x.split('un')[-1])
 
-Double_pre = os.popen('cat /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_5_2_7/src/DoubleMu/runlog_web').readlines()
+	for y in os.listdir(d+'/'+x): #loop over type files
+		NoSite = False
+		NoPNG = False
+		NoImages = False
+		#if 'Mu' not in y and 'xpress' not in y and 'osmic' not in y:
+		#print '*'*20,'  ',x
 
-os.system('cp /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_5_2_7/src/DoubleMu/runlog_web ~/CSCVAL/DOUBLE/')
+		if '.' in y:
+			continue
+		if ('Site' not in str(os.listdir(d+'/'+x+'/'+y))):#if no Site folder, Mark as Missing
+			NoSite = True
+		else:
+			if 'Site' in str(os.popen('ls '+d+'/'+x+'/'+y+'/Site').readlines()):# is Site a directory?
+				NoSite = True
+			else:
+				if 'PNGS' not in str(os.listdir(d+'/'+x+'/'+y+'/Site')):#if no PNG folder, Mark as Missing
+					NoPNG = True
+				else:
+					if '.png' not in str(os.listdir(d+'/'+x+'/'+y+'/Site/PNGS')): #if no pngs in PNG, Mark as Missing
+						NoImages = True
+						#continue
+		#if '206868' in x:
+		#	print run, 206868, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		print "######"
+		print run,NoSite,NoPNG,NoImages
+		print "######"
+		sumfile=d+'/'+x+'/'+y+'/Site/Summary.html'
+		if 'Summary.html' in str(os.popen('ls ' + d+'/'+x+'/'+y+'/Site/').readlines()): #check for summary file
+			if ('Run2012D' not in str(os.popen('cat '+sumfile).readlines())):#era parameter, if not in era, skip
+				continue				
+			info = os.popen('grep Processed '+sumfile).readlines() #number of events in run, no explicit purpose here
+			info=(info[0].split("style_2\">"))[-1]
+			info=(info.split('<'))[0]
+			info=int(info)
+		else:
+			info = -1
+		#print run,'in 2012D'	
+		type=y
+		if type not in types:
+			types.append(type)
+		#print run,y,info
+		if (NoSite):
+			allinfo.append([run,y,info,'Missing Site'])
+			print 'no site info appended'
+		else:
+			if (NoPNG):
+				allinfo.append([run,y,info,'Missing PNG'])
+				print 'no png dir info appended'
+			else:
+				if (NoImages):
+					allinfo.append([run,y,info,'Missing Images'])
+					print 'no images info appended',allinfo[-1]
+			
+#emptyfiles = open('listofemptyfiles_D.txt','w')
+#for a in allinfo: #Lists run folders with defects: missing Site or PNG or pngs
+	#if "Double" in str(a):
+		#emptyfiles.write(str(a) + '\n')
+#emptyfiles.write('\n')
+#for a in allinfo:
+	#if "Single" in str(a):
+		#emptyfiles.write(str(a) + '\n')
+#emptyfiles.write('\n')
+#for a in allinfo:
+	#if ("Single" not in str(a)) and ("Double" not in str(a)):
+		#emptyfiles.write(str(a) + '\n')
+#emptyfiles.close()
 
-Double_pre_str = str(Double_pre).replace('\\n','')
 
-exec('Double_pre ='+Double_pre_str)
 
-print Double_pre
+emptylist = open('runlist_empty.py','w')
+emptylist.write('DOUBLE=[]\n')
+for a in allinfo:
+	if "Double" in str(a):
+		emptylist.write('DOUBLE = DOUBLE+["'+str(a[0])+'"]\n')
 
-Double_post = [item for item in Double_pre if item not in DOUBLE]
-
-print Double_post
-
-doublelist = open('runlog_double','w')
-for a in Double_post:
-	doublelist.write(a+'\n')
-doublelist.close()
-
-os.system('mv runlog_double ~/CSCVAL/DOUBLE')
-
-############################################################################################
-
-Single_pre = os.popen('cat /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_5_2_7/src/SingleMu/runlog_web').readlines()
-
-os.system('cp /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_5_2_7/src/SingleMu/runlog_web ~/CSCVAL/SINGLE/')
-
-Single_pre_str = str(Single_pre).replace('\\n','')
-
-exec('Single_pre ='+Single_pre_str)
-
-print Single_pre
-
-Single_post = [item for item in Single_pre if item not in SINGLE]
-
-print Single_post
-
-singlelist = open('runlog_single','w')
-for a in Single_post:
-	singlelist.write(a+'\n')
-singlelist.close()
-
-os.system('mv runlog_single ~/CSCVAL/SINGLE')
+emptylist.write('\n\nSINGLE=[]\n')
+for a in allinfo:
+	if "Single" in str(a):
+		emptylist.write('SINGLE = SINGLE+["'+str(a[0])+'"]\n')
+emptylist.close()
+#next use DefectRemover, but first check runlist_empty.py
+	
+	
+	
+				
